981b96ad04498c3d6c696a60613e6b252de8c76c
İbrahim Ali METİN <ibrahimalimetin@gmail.com>
2019-11-01 00:24:54 +0300

    inception

---
+++ classify.py
@@ -0,0 +1,13 @@
+import inception
+
+inception.download()
+
+model = inception.Inception()
+
+def classify(image_path):
+    pred = model.classify(image_path=image_path)
+    model.print_scores(pred=pred, k=10, only_first_name=True)
+
+classify(image_path='images/4.jpg')
+
+
